ee95ef334f9f3fb753ec4610cb2e58e59598fb2a
vyrs <vitory.rsilva@hotmail.com>
2021-02-22 19:53:42 -0300

    primeiro commit

---
+++ src/main/java/br/com/alura/jpa/testes/CriaConta.java
@@ -0,0 +1,26 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class CriaConta {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
+
+        Conta conta = new Conta();
+        conta.setTitular("Vitor");
+        conta.setNumero(1235);
+        conta.setAgencia(4322);
+        conta.setSaldo(500.0);
+
+        em.getTransaction().begin();
+
+        em.persist(conta);
+
+        em.getTransaction().commit();
+    }
+}
